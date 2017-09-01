cc77263f2bbbb936b3cb0bc3549d2fd95c25544f
Riya Dennis <riyacv@gmail.com>
2017-09-01 18:39:52 +0100

    applying inheritance

--- Other/learning.go
+++ Other/learning.go
@@ -4,21 +4,20 @@ import (
 	"time"
 	"fmt"
 	"log"
-	"./reddit"
 )
 
 func main() {
-	log.Println("Starting the application")
+	log.Println("Starting the application\n")
 	start := time.Now()
-	items, err := reddit.Get("golang")
-	if err != nil {
-		log.Fatal(err.Error())
+	fmt.Printf("time %d\n", time.Hour+time.Since(start))
+	nums := [5]int{1,2,3,4,5}
+	fmt.Printf("Sum is %d \n",sum(nums))
+}
+
+func sum(i [5]int)(int){
+	var total = 0
+	for _, val := range i{
+		total += val
 	}
-	for _, item := range items {
-		fmt.Println(item)
-	}
-	fmt.Println(items)
-	year, month, day := time.Now().Date()
-	fmt.Printf("Today is %d %s %d", year, month, day)
-	fmt.Printf("time %d", time.Hour+time.Since(start))
+	return total
 }
